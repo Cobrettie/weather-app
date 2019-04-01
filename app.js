5c03b2bdfa5af42f769398cd66fe27ac067c4a3e
Cobrettie <cobrettiegarner3@gmail.com>
2019-03-31 23:40:36 -0400

    Pull temperature, timezone, degrees from API, display on web page

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.darksky.net/forecast/50146111b724c5f42c1206ee8e0de422/${lat},${long}`;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/50146111b724c5f42c1206ee8e0de422/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data); // Logs the information into the console
+                    const {
+                        temperature, // pulling temperature and summary from darksky API
+                        summary
+                    } = data.currently;
+
+                    // Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                })
         })
     } else {
         /* else statement, that displays when the user does not allow location to be known */
